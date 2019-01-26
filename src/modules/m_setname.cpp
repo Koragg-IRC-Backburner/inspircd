cf681f3b1d6477b8d296a4c0fed7d2902f5da1bb
Koragg <31944041+KoraggKnightWolf@users.noreply.github.com>
2019-01-26 11:41:57 +0100

    Give m_setname its own snomask char

--- src/modules/m_setname.cpp
+++ src/modules/m_setname.cpp
@@ -42,7 +42,7 @@ class CommandSetname : public Command
 
 		if (user->ChangeRealName(parameters[0]))
 		{
-			ServerInstance->SNO->WriteGlobalSno('a', "%s used SETNAME to change their real name to '%s'", user->nick.c_str(), parameters[0].c_str());
+			ServerInstance->SNO->WriteGlobalSno('s', "%s used SETNAME to change their real name to '%s'", user->nick.c_str(), parameters[0].c_str());
 		}
 
 		return CMD_SUCCESS;
